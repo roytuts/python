695802bd004ffe60f9e0558ede37cfa4d9406fac
jeejava roytuts <projwebinfo@gmail.com>
2019-05-01 07:35:15 +0530

    Add files via upload

---
+++ user_crud/main.py
@@ -0,0 +1,127 @@
+import pymysql
+from app import app
+from db_config import mysql
+from flask import jsonify
+from flask import flash, request
+from werkzeug import generate_password_hash, check_password_hash
+		
+@app.route('/add', methods=['POST'])
+def add_user():
+	try:
+		_json = request.json
+		_name = _json['name']
+		_email = _json['email']
+		_password = _json['pwd']
+		# validate the received values
+		if _name and _email and _password and request.method == 'POST':
+			#do not save password as a plain text
+			_hashed_password = generate_password_hash(_password)
+			# save edits
+			sql = "INSERT INTO tbl_user(user_name, user_email, user_password) VALUES(%s, %s, %s)"
+			data = (_name, _email, _hashed_password,)
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute(sql, data)
+			conn.commit()
+			resp = jsonify('User added successfully!')
+			resp.status_code = 200
+			return resp
+		else:
+			return not_found()
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+		
+@app.route('/users')
+def users():
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT user_id id, user_name name, user_email email, user_password pwd FROM tbl_user")
+		rows = cursor.fetchall()
+		resp = jsonify(rows)
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+		
+@app.route('/user/<int:id>')
+def user(id):
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT user_id id, user_name name, user_email email, user_password pwd FROM tbl_user WHERE user_id=%s", id)
+		row = cursor.fetchone()
+		resp = jsonify(row)
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.route('/update', methods=['PUT'])
+def update_user():
+	try:
+		_json = request.json
+		_id = _json['id']
+		_name = _json['name']
+		_email = _json['email']
+		_password = _json['pwd']		
+		# validate the received values
+		if _name and _email and _password and _id and request.method == 'PUT':
+			#do not save password as a plain text
+			_hashed_password = generate_password_hash(_password)
+			# save edits
+			sql = "UPDATE tbl_user SET user_name=%s, user_email=%s, user_password=%s WHERE user_id=%s"
+			data = (_name, _email, _hashed_password, _id,)
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute(sql, data)
+			conn.commit()
+			resp = jsonify('User updated successfully!')
+			resp.status_code = 200
+			return resp
+		else:
+			return not_found()
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+		
+@app.route('/delete/<int:id>', methods=['DELETE'])
+def delete_user(id):
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor()
+		cursor.execute("DELETE FROM tbl_user WHERE user_id=%s", (id,))
+		conn.commit()
+		resp = jsonify('User deleted successfully!')
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+		
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+		
+if __name__ == "__main__":
+    app.run()
