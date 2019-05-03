0ac548b640b0b4f82e6684110a9262f4451f47a4
jeejava roytuts <projwebinfo@gmail.com>
2019-05-03 12:38:20 +0530

    Add files via upload

---
+++ real_time_eye_detection/eye_detection.py
@@ -0,0 +1,41 @@
+import cv2
+
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+eyeCascade= cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# grab the reference to the webcam
+vs = cv2.VideoCapture(0)
+
+# keep looping
+while True:
+	# grab the current frame
+	ret, frame = vs.read()
+  
+	# if we are viewing a video and we did not grab a frame,
+	# then we have reached the end of the video
+	if frame is None:
+		break
+		
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.equalizeHist(gray)
+		
+	faces = faceCascade.detectMultiScale(frame)
+
+	for (x,y,w,h) in faces:
+		#cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+		roi_gray = gray[y:y+h, x:x+w]
+		roi_color = frame[y:y+h, x:x+w]
+		eyes = eyeCascade.detectMultiScale(roi_gray)
+		for (ex,ey,ew,eh) in eyes:
+			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+	
+	# show the frame to our screen
+	cv2.imshow("Video", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the 'q' key is pressed, stop the loop
+	if key == ord("q"):
+		break
+ 
+# close all windows
+cv2.destroyAllWindows()
