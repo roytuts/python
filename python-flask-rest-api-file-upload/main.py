8e93fffaa539911b6bb5ccc06114f1dc6ac50bd4
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-09-19 18:09:46 +0530

    Update main.py

--- python-flask-rest-api-file-upload/main.py
+++ python-flask-rest-api-file-upload/main.py
@@ -11,27 +11,26 @@ def allowed_file(filename):
 
 @app.route('/file-upload', methods=['POST'])
 def upload_file():
-	if request.method == 'POST':
-        # check if the post request has the file part
-		if 'file' not in request.files:
-			resp = jsonify('No file part in the request')
-			resp.status_code = 500
-			return resp
-		file = request.files['file']
-		if file.filename == '':
-			resp = jsonify('No file selected for uploading')
-			resp.status_code = 500
-			return resp
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			resp = jsonify('File successfully uploaded')
-			resp.status_code = 200
-			return resp
-		else:
-			resp = jsonify('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
-			resp.status_code = 500
-			return resp
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		resp = jsonify({'message' : 'No file part in the request'})
+		resp.status_code = 400
+		return resp
+	file = request.files['file']
+	if file.filename == '':
+		resp = jsonify({'message' : 'No file selected for uploading'})
+		resp.status_code = 400
+		return resp
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		resp = jsonify({'message' : 'File successfully uploaded'})
+		resp.status_code = 201
+		return resp
+	else:
+		resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+		resp.status_code = 500
+		return resp
 
 if __name__ == "__main__":
     app.run()
