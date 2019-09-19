25e37d52a74e5088e950f76824b91d732e32683a
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-09-19 12:31:57 +0530

    Add files via upload

---
+++ python-flask-rest-api-file-upload/main.py
@@ -0,0 +1,37 @@
+import os
+import urllib.request
+from app import app
+from flask import Flask, request, redirect, jsonify
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/file-upload', methods=['POST'])
+def upload_file():
+	if request.method == 'POST':
+        # check if the post request has the file part
+		if 'file' not in request.files:
+			resp = jsonify('No file part in the request')
+			resp.status_code = 500
+			return resp
+		file = request.files['file']
+		if file.filename == '':
+			resp = jsonify('No file selected for uploading')
+			resp.status_code = 500
+			return resp
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			resp = jsonify('File successfully uploaded')
+			resp.status_code = 200
+			return resp
+		else:
+			resp = jsonify('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+			resp.status_code = 500
+			return resp
+
+if __name__ == "__main__":
+    app.run()
