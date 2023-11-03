df4dac38a87824f7c2d8d4e3fffc4b3bb3cd2df2
Soumitra <projwebinfo@gmail.com>
2023-11-03 15:35:15 +0530

    Delete flask-rest-mongodb directory
    moved to https://github.com/roytuts/flask/tree/master/flask-rest-mongodb

--- flask-rest-mongodb/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-
-app = Flask(__name__)
-app.secret_key = "secret key"
-app.config["MONGO_URI"] = "mongodb://localhost:27017/roytuts"
-mongo = PyMongo(app)
--- flask-rest-mongodb/readme.rst
+++
@@ -1,1 +0,0 @@
-Please follow the tutorial https://roytuts.com/flask-rest-api-mongodb-crud-example/
